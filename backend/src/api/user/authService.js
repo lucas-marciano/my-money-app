e93ccb5bc88e69ab0f638c6a27fc9ac6f0be8efb
Lucas Marciano <lucasmarciano@inatel.br>
2019-06-04 16:39:26 -0300

    (refact): update the libs of the front end backend.

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -2,7 +2,7 @@ const _ = require('lodash')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('./user')
-const env = require('../../.env')
+const env = require('../../../.env')
 
 const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
@@ -21,7 +21,7 @@ const login = (req, res, next) => {
         if (err) {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign(user, env.authSecret, {
+            const token = jwt.sign({ ...user }, env.authSecret, {
                 expiresIn: "1 day"
             })
             const { name, email } = user
